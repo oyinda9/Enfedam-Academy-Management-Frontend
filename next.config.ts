d4458946e537b33e3e3556f10a90b128abcf041d
sholanke precious <sholankeprecious3@gmail.com>
2025-03-26 12:31:06 +0100

    Ignore TypeScript and ESLint errors

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true, // Ignores ESLint errors
+  },
 };
 
 export default nextConfig;
