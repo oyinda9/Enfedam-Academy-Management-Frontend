d51dfabae9f086fc2d2994f1771c0e448c1b9ac1
sholanke precious <sholankeprecious3@gmail.com>
2025-06-14 21:25:26 +0100

    please

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,8 +1,13 @@
-"use client"
+"use client";
+
 import React from "react";
-import Announcements from "@/components/Announcements";
-import EventCalendar from "@/components/EventCalendar";
-import BigCalendar from "@/components/BigCalendar";
+import dynamic from "next/dynamic";
+
+// Dynamically import components with SSR disabled
+const BigCalendar = dynamic(() => import("@/components/BigCalendar"), { ssr: false });
+const EventCalendar = dynamic(() => import("@/components/EventCalendar"), { ssr: false });
+const Announcements = dynamic(() => import("@/components/Announcements"), { ssr: false });
+
 const Studentpage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col xl:flex-row font-poppins">
@@ -10,8 +15,7 @@ const Studentpage = () => {
       <div className="w-full xl:w-2/3">
         <div className="h-full bg-white p-4 rounded-md">
           <h1 className="text-xl font-semibold">Schedule (41)</h1>
-          <BigCalendar/>
-
+          <BigCalendar />
         </div>
       </div>
 
